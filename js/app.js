5ff5a5287fdc3e251ad9b27555c3fd65e3abc9ef
manu <manu.villar.vitoria@gmail.com>
2020-04-30 15:02:18 +0200

    adding courses to cart and reading their data in HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //Variables
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody');
 
 
 //Listeners
@@ -18,7 +19,7 @@ function comprarCurso(e) {
     e.preventDefault();
     //Delegation para agregar carrito
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log('Si');
+        //console.log('Si');
         const curso = e.target.parentElement.parentElement;
         //Enviamos el curso seleccionado para tomar sus datos
         leerDatosCurso(curso);
@@ -26,5 +27,30 @@ function comprarCurso(e) {
 }
 
 function leerDatosCurso(curso) {
-    console.log(curso);
+    //console.log(curso);
+    //ponemos los datos en un objeto
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito(infoCurso);
+}
+
+//Muestra el curso seleccionado en el carrito del HTML
+function insertarCarrito(curso) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src= "${curso.imagen}" width=100>
+        </td>
+        <td>"${curso.titulo}"></td>
+        <td>"${curso.precio}"></td>
+        <td>
+        <a href= "#" class="borrar-curso"  data-id= "${curso.id}
+        "> X </a>
+        </td>
+    `;
+    listaCursos.appendChild(row);
 }
