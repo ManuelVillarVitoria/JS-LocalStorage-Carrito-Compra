c57e378c93034a77d75f8e134fdc9a60b70716bb
manu <manu.villar.vitoria@gmail.com>
2020-04-30 22:04:16 +0200

    printing LocalStorage courses when loading the website

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ function cargaEventListeners(){
 
     //Al vaciar el carrito
     vaciarCarritoBtn.addEventListener('click',vaciarCarrito);
+
+    //Al cargar el documento mostrar el LocalStorage
+    document.addEventListener('DOMContentLoaded', leerLocalStorage);
 }
 
 
@@ -114,5 +117,29 @@ function obtenerCursosLocalStorage(){
        cursosLS = JSON.parse(localStorage.getItem('cursos'));
     }
     return cursosLS;
+}
+
+//Imprime los cursos del LocalStotage en el carrito
+function leerLocalStorage() {
+    let cursosLS;
+
+    cursosLS = obtenerCursosLocalStorage();
+    //console.log(cursosLS);
+
+    cursosLS.forEach(function(curso) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src= "${curso.imagen}" width=100>
+            </td>
+            <td>"${curso.titulo}"></td>
+            <td>"${curso.precio}"></td>
+            <td>
+            <a href= "#" class="borrar-curso"  data-id= "${curso.id}
+            "> X </a>
+            </td>
+        `;
+        listaCursos.appendChild(row);
+    });
 }
 
